7b2b36808ea2f6503ac0d2c3ed23c8bf75f0d629
Huy317 <167590855+Huy317@users.noreply.github.com>
2025-05-01 13:35:17 +0700

    added Q2

---
+++ Q2Login/Login.js
@@ -0,0 +1,27 @@
+import {View, Text, TextInput, ScrollView, TouchableOpacity} from 'react-native';
+import Styles from './style';
+
+const LoginScreen = () => {
+    return(
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={Styles.container}>
+                <Text style={Styles.title}>Login</Text>
+                <TextInput
+                    style={Styles.input}
+                    placeholder='Phone'
+                />
+                <TextInput
+                    style={Styles.input}
+                    placeholder='Password'
+                    secureTextEntry
+                />
+                <TouchableOpacity style={Styles.Button}>
+                    <Text style={Styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+            </View>
+
+        </ScrollView>
+    );
+};
+
+export default LoginScreen;
